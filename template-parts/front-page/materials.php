e86b839bff9b93c7cbd31b825115670bb337ecbe
wjaag <wjag@onet.pl>
2026-09-08 01:29:55 +0200

    refactor: extract front page materials template part

---
+++ template-parts/front-page/materials.php
@@ -0,0 +1,11 @@
+<?php
+/** Front page materials strip. */
+?>
+<section class="hg-material-strip" aria-label="Materiały stosowane w studio">
+    <div class="hg-container">
+        <p>Pracujemy na sprawdzonych systemach</p>
+        <div class="hg-material-list">
+            <span>3M <b>2080</b></span><span>AVERY <b>DENNISON</b></span><span>HEXIS</span><span>KPMF</span><span>ORACAL</span><span>XPEL</span>
+        </div>
+    </div>
+</section>
